eba82d242969a20a9bed892ccc822c33e145373b
nak435 <42625437+nak435@users.noreply.github.com>
2026-04-17 21:15:11 +0900

    Add keyboard key codes to TinyUSB_Mouse_Keyboard.h

--- demo_hid_keyboard_mouse/TinyUSB_Mouse_Keyboard.h
+++ demo_hid_keyboard_mouse/TinyUSB_Mouse_Keyboard.h
@@ -68,4 +68,77 @@ extern "C" {
     void usb_handle_user_data( struct usb_endpoint * e, int current_endpoint, uint8_t * data, int len, struct rv003usb_internal * ist );
 }
 
+//  Keyboard codes
+
+#define KEY_LEFT_CTRL       0x80
+#define KEY_LEFT_SHIFT      0x81
+#define KEY_LEFT_ALT        0x82
+#define KEY_LEFT_GUI        0x83
+#define KEY_LEFT_WINDOWS    0x83    //alias for Windows
+#define KEY_LEFT_COMMAND    0x83    //alias for macOS
+#define KEY_RIGHT_CTRL      0x84
+#define KEY_RIGHT_SHIFT     0x85
+#define KEY_RIGHT_ALT       0x86
+#define KEY_RIGHT_GUI       0x87
+#define KEY_RIGHT_WINDOWS   0x87    //alias for Windows
+#define KEY_RIGHT_COMMAND   0x87    //alias for macOS
+
+#define KEY_RETURN          0xB0
+#define KEY_ESC             0xB1
+#define KEY_BACKSPACE       0xB2
+#define KEY_TAB             0xB3
+#define KEY_CAPS_LOCK       0xC1
+#define KEY_F1              0xC2
+#define KEY_F2              0xC3
+#define KEY_F3              0xC4
+#define KEY_F4              0xC5
+#define KEY_F5              0xC6
+#define KEY_F6              0xC7
+#define KEY_F7              0xC8
+#define KEY_F8              0xC9
+#define KEY_F9              0xCA
+#define KEY_F10             0xCB
+#define KEY_F11             0xCC
+#define KEY_F12             0xCD
+#define KEY_F13             0xF0
+#define KEY_F14             0xF1
+#define KEY_F15             0xF2
+#define KEY_F16             0xF3
+#define KEY_F17             0xF4
+#define KEY_F18             0xF5
+#define KEY_F19             0xF6
+#define KEY_F20             0xF7
+#define KEY_F21             0xF8
+#define KEY_F22             0xF9
+#define KEY_F23             0xFA
+#define KEY_F24             0xFB
+#define KEY_INSERT          0xD1
+#define KEY_HOME            0xD2
+#define KEY_PAGE_UP         0xD3
+#define KEY_DELETE          0xD4
+#define KEY_END             0xD5
+#define KEY_PAGE_DOWN       0xD6
+#define KEY_RIGHT_ARROW     0xD7
+#define KEY_LEFT_ARROW      0xD8
+#define KEY_DOWN_ARROW      0xD9
+#define KEY_UP_ARROW        0xDA        
+#define KEY_KEYPAD_DIVIDE   0xDC
+#define KEY_KEYPAD_MULTIPLY 0xDD
+#define KEY_KEYPAD_SUBTRACT 0xDE
+#define KEY_KEYPAD_ADD      0xDF
+#define KEY_KEYPAD_ENTER    0xE0
+#define KEY_KEYPAD_1        0xE1
+#define KEY_KEYPAD_2        0xE2
+#define KEY_KEYPAD_3        0xE3
+#define KEY_KEYPAD_4        0xE4
+#define KEY_KEYPAD_5        0xE5
+#define KEY_KEYPAD_6        0xE6
+#define KEY_KEYPAD_7        0xE7
+#define KEY_KEYPAD_8        0xE8
+#define KEY_KEYPAD_9        0xE9
+#define KEY_KEYPAD_0        0xEA
+#define KEY_KEYPAD_DECIMAL  0xEB
+#define KEY_KEYPAD_EQUAL    0xEF
+
+
 #endif
